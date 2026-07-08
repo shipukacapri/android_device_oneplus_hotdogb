46ca8af84a11e545932315fd40be1d02eab6c886
jihad <jhaidh277@gmail.com>
2026-07-08 11:20:31 +0600

    Change lunch combo from InfinityX to Evolution

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 # InfinityX Build Setup for OnePlus 7T (hotdogb)
 for variant in user userdebug eng; do
-    add_lunch_combo infinity_hotdogb-$variant
+    add_lunch_combo evolution_hotdogb-$variant
 done
