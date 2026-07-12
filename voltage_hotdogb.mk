1ad27a78127cb41f9b2fbc1ac9a03c1d07bccf14
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-07-12 14:48:32 +0530

    hotdogb: to voltag

--- voltage_hotdogb.mk
+++ voltage_hotdogb.mk
@@ -12,11 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hotdogb device
 $(call inherit-product, device/oneplus/hotdogb/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+
+# Voltage Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hotdogb
+PRODUCT_NAME := voltage_hotdogb
 PRODUCT_DEVICE := hotdogb
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := HD1901
